bb2f110ef67b9be796532e8b12a104764f73904e
Paul Rupke <paul.rupke@gmail.com>
2023-06-19 22:10:20 -0400

    add basic error page

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,30 @@
+import { useRouteError } from "react-router-dom";
+import './ErrorPage.scss';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  const serverErrorMessage = "Sorry, an unexpected server error has occurred.";
+
+  const statusCodeToMessageMap = {
+    400: 'Hmm... the server did not like your request.',
+    403: 'You are not authorized to view this page.',
+    404: 'This route does not exist.',
+    500: serverErrorMessage,
+    501: serverErrorMessage,
+    502: serverErrorMessage,
+    504: serverErrorMessage,
+  }
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>{statusCodeToMessageMap[error.status]}</p>
+      <p>
+        Status code: <b>{error.status} </b>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
