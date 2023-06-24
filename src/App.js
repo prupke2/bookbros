76d6dda727132881f147bbe052325a62a52a0491
Paul Rupke <paul.rupke@gmail.com>
2023-06-24 13:29:12 -0400

    initialize parse in app

--- src/App.js
+++ src/App.js
@@ -2,29 +2,22 @@ import React from 'react';
 import './App.scss';
 import Home from './Home/Home';
 import { Routes, Route } from "react-router-dom";
-// import Books from './Books/Books';
-// import Parse from 'parse/dist/parse.min.js';
-import Parse from 'parse/dist/parse.min.js';
-import Books from './Books/Books';
 
-// import { initializeParse } from '@parse/react';
+import { initializeParse } from '@parse/react';
+import BookSearch from './BookSearch/BookSearch';
 
-// initializeParse(
-//   'https://bookbros.b4a.io/',
-//   'j8bZXyteOFiSCWNItcLmRfdimaPOSdZF9vBPKQFv',
-//   'YZDR2jPHJKrHmP2UjtT0l6D6jPvo7SsH2qSIJdU8'
-// );
+initializeParse(
+  'https://bookbros.b4a.io/',
+  'j8bZXyteOFiSCWNItcLmRfdimaPOSdZF9vBPKQFv',
+  'YZDR2jPHJKrHmP2UjtT0l6D6jPvo7SsH2qSIJdU8'
+);
 
 function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<Home />}>
-          {/* <Route path="home" element={<Home />} /> */}
-
-      </Route>
-      <Route path="books" element={<Books />} />
-
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<BookSearch />} />
     </Routes>
 
     // <Home />
