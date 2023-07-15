5a205c8da2a2187c9690e67c1a8608d89e047bbc
Paul Rupke <paul.rupke@gmail.com>
2023-07-15 14:18:28 -0400

    parse ratings, add pagination

--- src/Books/hooks.js
+++ src/Books/hooks.js
@@ -45,22 +45,55 @@ Sample results:
 	]
 */
 
-export const getBooks = async (clubId) => {
-	const query = new Parse.Query('Books').contains('club', `${clubId}`).descending('created_at');
+const parseRatings = ratings => {
+	let bookRatings = [];
+	let ratingData = {};
+	try {
+		ratings.forEach(rating => {
+			ratingData.book_id = rating.get("book_id");
+			ratingData.rating = rating.get("rating");
+			ratingData.name = rating.get("name");
+			bookRatings.push(ratingData);
+			ratingData = {}
+		});
+	} catch (err) {
+		console.log('error parsing ratings:', err);
+	}
+	return bookRatings;
+}
+
+const paginateParseObject = async (query) => {
+	let count = 0;
 	const results = await query.find();
+	let objectArray = [...results];
+	while (objectArray.length % 100 === 0) {
+		count += 100;
+		query.skip(count);
+		const newResults = await query.find();
+		objectArray = [...objectArray, ...newResults];
+	}
+	return objectArray;
+}
+
+export const getBooks = async (clubId) => {
+	const query = new Parse.Query('Books').equalTo('club', `${clubId}`).descending('created_at');
+	const results = await paginateParseObject(query);
 	return results;
 };
 
 export const getRatings = async (clubId) => {
-	const query = new Parse.Query('Ratings').contains('club', `${clubId}`).descending('createdAt');
-	const results = await query.find();
-	return results;
+	const query = new Parse.Query('Ratings').equalTo('club', `${clubId}`).descending('rating');
+	const ratings = await paginateParseObject(query);
+	return parseRatings(ratings)
 };
 
 export const getRatingsForBook = async (bookId, clubId) => {
-	const query = new Parse.Query('Ratings').contains('club', `${clubId}`).contains('book_id', bookId).descending('createdAt');
-	const bookRatings = await query.find();
-	return bookRatings;
+	const query = new Parse.Query('Ratings').equalTo('club', `${clubId}`).equalTo('book_id', bookId).descending('rating');
+	const ratings = await paginateParseObject(query);
+	return parseRatings(ratings)
+	// const results = await query.find();
+	// const bookRatings = parseRatings(results);
+	// return bookRatings;
 };
 
 export const getBook = async (objectId) => {
