9a3653e6ca3c559eb9dd8700e0dd054091020903
Paul Rupke <paul.rupke@gmail.com>
2024-01-28 13:47:02 -0500

    clean up books hooks, move Parse info

--- src/Books/hooks.js
+++ src/Books/hooks.js
@@ -1,51 +1,6 @@
 import Parse from 'parse';
 import { transformBookFetchResults } from './utils';
 
-/* Parse tl;dr:
-
-	const {
-		isLive, // Indicates that Parse Live Query is connected
-		isLoading, // Indicates that the initial load is being processed
-		isSyncing, // Indicates that the library is getting the latest data from Parse Server
-		results, // Stores the current results in an array of Parse Objects
-		count, // Stores the current results count
-		error, // Stores any error
-		reload // Function that can be used to reload the data
-	} = useParseQuery(
-		parseQuery, // The Parse Query to be used
-		{
-			enabled: true, // Enables the parse query (default: true)
-			enableLocalDatastore: true, // Enables cache in local datastore (default: true)
-			enableLiveQuery: true // Enables live query for real-time update (default: true)
-		}
-
---------------------------
-
-Sample results:
-	[
-		{
-				"createdAt": "2023-04-23T17:15:02.183Z",
-				"updatedAt": "2023-04-23T17:15:02.183Z",
-				"book_id": "QfbS_rl5VsoC",
-				"title": "Strangers on a Train",
-				"author": "Patricia Highsmith",
-				"club": "BRIjNbcPR9",
-				"user": "Andrew",
-				"objectId": "7XMpAeXYVi"
-		},
-		{
-				"createdAt": "2023-04-23T17:15:02.183Z",
-				"updatedAt": "2023-04-23T17:15:02.183Z",
-				"book_id": "4ZQnDwAAQBAJ",
-				"title": "Conversations with Friends",
-				"author": "Sally Rooney",
-				"club": "BRIjNbcPR9",
-				"user": "Mike",
-				"objectId": "kEi4Um89JY"
-		}
-	]
-*/
-
 export const parseRatings = ratings => {
 	let bookRatings = [];
 	let ratingData = {};
@@ -83,13 +38,6 @@ export const paginateParseObject = async (query) => {
 }
 
 export const getBooks = async (clubId) => {
-	// const booksInLocalStorage = JSON.parse(localStorage.getItem('books')) || null;
-	// const booksUpdated = localStorage.getItem('booksUpdated') || null;
-	// const tenMinutesAgo = Date.now() - 600000;
-	// // only refresh books if they are not in local storage or if they are older than 10 minutes
-	// if (booksInLocalStorage && (!forceRefresh || (booksUpdated && booksUpdated > tenMinutesAgo)) ) {
-	// 	return booksInLocalStorage;
-	// }
 	const query = new Parse.Query('Books').equalTo('club', `${clubId}`).descending('created_at');
 	const results = await paginateParseObject(query);
 	const books = await transformBookFetchResults(results);
