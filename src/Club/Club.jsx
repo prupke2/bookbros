39a44ef7e3725217f3f3e20b7e1553f2aaeaa21d
Paul Rupke <paul.rupke@gmail.com>
2024-03-10 13:20:58 -0400

    add new list components

--- src/Club/Club.jsx
+++ src/Club/Club.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import {
 	getHighestOwnBookRatings,
+	getHighestRatedBooks,
+	getLowestRatedBooks,
 	getMeanBookAverageRating,
 	getMeanOfListOfFloats,
 	getUserAverageRatings,
@@ -15,11 +17,17 @@ import './Club.scss';
 const Club = ({ brand }) => {
 	const books = JSON.parse(localStorage.getItem('books')) || [];
 	const ratings = JSON.parse(localStorage.getItem('ratings')) || [];
-
 	const transformedData = transformChartData(books);
+
+	const numberOfBooks = books.length;
+	const listCount = numberOfBooks > 5 ? 5 : numberOfBooks;
+	const highestRatedBooks = transformedData && getHighestRatedBooks(books, listCount);
+	const lowestRatedBooks = transformedData && getLowestRatedBooks(books, listCount);
+
 	const usersWithRatedBooks = transformedData.map(item => item.id);
 	const userAverageRatings = transformedData && getUserAverageRatings(transformedData);
 	const highestOwnBookRatings = transformedData && getHighestOwnBookRatings(usersWithRatedBooks, books, ratings);
+
 	const meanBookAverageRating = getMeanBookAverageRating();
 	const booksButNoRatings = books.length && !transformedData.length;
 	const meanRating = getMeanOfListOfFloats(ratings.map(rating => rating.rating));
@@ -47,14 +55,27 @@ const Club = ({ brand }) => {
 							<List
 								data={[{ averageRating: meanRating }]}
 								title="Average Rating Given"
+								type="rating"
 							/>
 							<div className='lists-wrapper'>
-								{ userAverageRatings && (
-									<List 
-										data={userAverageRatings} 
-										title="Highest Average Rating" 
-									/>
-								)}
+								<List 
+									data={highestRatedBooks} 
+									title="Highest Rated Books"
+									type="book"
+								/>
+								{ numberOfBooks > 5 && (
+									<List
+										data={lowestRatedBooks}
+										title="Lowest Rated Books"
+										type="book"
+										/>
+								) }
+							</div>
+							<div className='lists-wrapper'>
+								<List 
+									data={userAverageRatings} 
+									title="Highest Average Rating" 
+								/>
 								{ highestOwnBookRatings && (
 									<List 
 										data={highestOwnBookRatings} 
