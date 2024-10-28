38ef7ea5b747feba6c83c94ae8a0ec64d023d4cf
Paul Rupke <paul.rupke@gmail.com>
2024-10-27 20:28:59 -0400

    add two utils

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ export const isLocalHost = () => (window.location.hostname === "localhost" || wi
 
 export const coerceToString = data => typeof(data) === 'string' ? data : data.join(', ');
 
+export const twoDecimalDigits = (num) => (Math.round(num * 100) / 100).toFixed(2);
+
+export const truncateString = (string, maxLength) => string.substring(0, maxLength);
+
 export const calculateAverageRating = ratings => {
 	let total = 0;
 	ratings.forEach(r => {
