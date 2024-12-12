de53ee6e8553df5be1434f0fd25c8223a66fd8d5
Paul Rupke <paul.rupke@gmail.com>
2024-12-11 20:31:56 -0500

    fix utils bug

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-export const isLocalHost = () => (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
+export const isLocalHost = () => (window.location.hostname.includes("localhost", "127.0.0.1"));
 
 export const coerceToString = data => typeof(data) === 'string' ? data : data.join(', ');
 
 export const twoDecimalDigits = (num) => (Math.round(num * 100) / 100).toFixed(2);
 
-export const truncateString = (string, maxLength) => string.substring(0, maxLength);
+export const truncateString = (string, maxLength) => string?.substring(0, maxLength);
 
 export const calculateAverageRating = ratings => {
 	let total = 0;
